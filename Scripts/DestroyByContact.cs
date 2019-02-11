5513c98b54df39ebfe36af2611f418298fc628f2
geoblazer <31526887+geoyau@users.noreply.github.com>
2019-02-11 16:53:21 -0500

    Add files via upload

---
+++ Scripts/DestroyByContact.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour 
+{
+
+	public GameObject explosion;
+	public GameObject playerexplosion;
+	public int scoreValue;
+	private GameController gameController;
+
+	void Start ()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null) {
+			gameController = gameControllerObject.GetComponent <GameController> ();
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Boundary") 
+		{
+			return;
+		}
+		Instantiate (explosion, transform.position, transform.rotation);
+		if (other.tag == "Player") 
+		{
+			Instantiate (playerexplosion, other.transform.position, other.transform.rotation);
+			gameController.GameOver ();
+		}
+		gameController.Addscore (scoreValue);
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+}
---
+++ Scripts/RandomRotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+	public float tumble;
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+		rb.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+}
